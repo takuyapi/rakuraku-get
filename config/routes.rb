4d9a486cd875f4af751016d866ef62696ff93cc6
takuyapi <yakutaneyama@gmail.com>
2021-02-24 17:27:21 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :stores do
+        resources :foods, only: %i[index]
+      end
+      resources :temporary_orders, only: %i[index create]
+      put 'temporary_orders/replace', to: 'temporary_orders#replace'
+      resources :orders, only: %i[create]
+    end
+  end
 end
