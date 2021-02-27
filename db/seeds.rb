cc3142565696ead48b4aba9419e8abf559cf5a8e
takuyapi <yakutaneyama@gmail.com>
2021-02-27 22:47:26 +0900

    DBにデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+stores = ["肉屋", "魚屋", "八百屋"]
+foods = [["豚肉細切れ", "若どりもも肉2枚","若どり胸肉2枚"], ["鯖", "鮭"],["玉ねぎ","長ネギ"]]
+prefectures = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県",
+  "山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
+num = 0
+
+stores.each do |s|
+  store = Store.new(
+    name: s
+  )
+  foods[num].each do |food|
+    store.foods.build(
+      name: food,
+      price: rand(100..400),
+      origin: prefectures.sample,
+      description: if num != 2
+                    "#{rand(300..500) }グラム"
+                   else
+                    "#{rand(1..3)}個入り"
+                   end
+)
+  end
+  num += 1
+  store.save!
+end
+
